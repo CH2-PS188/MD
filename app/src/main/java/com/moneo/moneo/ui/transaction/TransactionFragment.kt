fe020388784e59143b54efe82d8f53230a638bec
rhadityaa <hdwrhaditya34@gmail.com>
2023-12-05 17:27:47 +0700

    test

--- app/src/main/java/com/moneo/moneo/ui/transaction/TransactionFragment.kt
+++ app/src/main/java/com/moneo/moneo/ui/transaction/TransactionFragment.kt
@@ -2,10 +2,15 @@ package com.moneo.moneo.ui.transaction
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.moneo.moneo.ViewModelFactory
+import com.moneo.moneo.adapter.TransactionAdapter
 import com.moneo.moneo.databinding.FragmentTransactionBinding
 
 class TransactionFragment : Fragment() {
@@ -24,6 +29,25 @@ class TransactionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val factory: ViewModelFactory = ViewModelFactory.getInstance(requireActivity())
+        val viewModel: TransactionViewModel by viewModels {
+            factory
+        }
+
+        val transactionAdapter = TransactionAdapter()
+
+        viewModel.getAllTransaction().observe(viewLifecycleOwner) { result ->
+            if (result != null) {
+                transactionAdapter.submitList(result)
+            }
+        }
+
+        binding?.rvTransaction?.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = transactionAdapter
+        }
 
         binding?.btnFab?.setOnClickListener {
             val intent = Intent(activity, AddTransactionActivity::class.java)
